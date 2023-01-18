0a37a26a21c5a9d9555803d02c1496a7a5fd69ab
wiktor34306 <92991883+wiktor34306@users.noreply.github.com>
2023-01-18 22:30:08 +0100

    tenth_part

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgxUiLoaderConfig, SPINNER } from 'ngx-ui-loader';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Frontend';
+}
