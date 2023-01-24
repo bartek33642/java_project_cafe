cce7257cac1498e9e0832a1eb64ed0dadd68e0cf
Agnieszka <agnieszka.derus@interia.pl>
2023-01-24 01:34:22 +0100

    16

--- Frontend/src/app/material-component/material.routing.ts
+++ Frontend/src/app/material-component/material.routing.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { RouteGuardService } from '../services/route-guard.service';
 import { ManageCategoryComponent } from './manage-category/manage-category.component';
+import {ManageProductComponent} from "./manage-product/manage-product.component";
 
 
 export const MaterialRoutes: Routes = [
@@ -12,5 +13,13 @@ export const MaterialRoutes: Routes = [
         data:{
             expectedRole: ['admin']
         }
+    },
+  {
+    path: 'product',
+    component: ManageProductComponent,
+    canActivate:[RouteGuardService],
+    data:{
+      expectedRole: ['admin']
     }
+  }
 ];
